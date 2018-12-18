2cf5e0ba87e28563798905295fa320e777e07600
Nicholas McClay <nmcclay@wombatsecurity.com>
2018-12-18 12:34:55 -0500

    Base commit with routable engine and addon-test component dependency

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const EngineAddon = require('ember-engines/lib/engine-addon');
 module.exports = EngineAddon.extend({
   name: 'engine-alpha',
 
-  lazyLoading: {
-    enabled: false
-  },
+  lazyLoading: Object.freeze({
+    enabled: true
+  }),
 
   isDevelopingAddon() {
     return true;
